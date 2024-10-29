9108729bd68148131e0b6601bc3b382656415c19
jdmax15 <joel.maxwell15@gmail.com>
2024-10-29 12:07:40 +1100

    First Commit

---
+++ renameDates.py
@@ -0,0 +1,6 @@
+#! python3
+# renameDates.py - Renames filenames with Maerican MM-DD-YYYY date format to European DD-MM-YYYY format.
+
+import shutil, os, re
+# Create a regex that matches files iwth the American date format.
+
